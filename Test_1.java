42c21d3787da201adb955590d9e997a976562661
kasaraji <166603083+kasaraji@users.noreply.github.com>
2024-04-19 10:16:51 +0530

    Update Test_1.java

--- Test_1.java
+++ Test_1.java
@@ -5,6 +5,7 @@ public class Test_1 {
 	public static void main(String[] args) {
 		System.out.println("Hello krish");
 		System.out.println("hello software");
+		System.out.println("Hii");
 		// TODO Auto-generated method stub
 
 	}
